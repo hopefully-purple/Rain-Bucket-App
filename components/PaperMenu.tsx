6c6c368c3748d3cab8e58fe8bb08bf81c0f01270
hopefully-purple <hopefully.purple@gmail.com>
2025-03-06 16:01:41 -0800

    implemented PaperMenu functionality

--- components/PaperMenu.tsx
+++ components/PaperMenu.tsx
@@ -1,14 +1,30 @@
 import Colors from "@/assets/colors/colors";
 import * as React from "react";
 import { View } from "react-native";
-import { Menu, IconButton } from "react-native-paper";
+import {
+  Menu,
+  IconButton,
+  Portal,
+  Dialog,
+  Button,
+  Text,
+} from "react-native-paper";
+
+const dialogText =
+  "The Add button will allow the user to add a new Language. The Edit button will allow the user to customize the list ordering, delete, rename, even set a color theme for each language";
 
 const PaperMenu = () => {
-  const [visible, setVisible] = React.useState(false);
+  const [menuVisible, setMenuVisible] = React.useState(false);
+  const [dialogVisible, setDialogVisible] = React.useState(false);
 
-  const openMenu = () => setVisible(true);
+  const openMenu = () => setMenuVisible(true);
+  const closeMenu = () => setMenuVisible(false);
 
-  const closeMenu = () => setVisible(false);
+  const showDialog = () => setDialogVisible(true);
+  const hideDialog = () => {
+    setDialogVisible(false);
+    setMenuVisible(false);
+  };
 
   return (
     <View
@@ -18,7 +34,7 @@ const PaperMenu = () => {
       }}
     >
       <Menu
-        visible={visible}
+        visible={menuVisible}
         onDismiss={closeMenu}
         anchor={
           <IconButton
@@ -28,21 +44,20 @@ const PaperMenu = () => {
           />
         }
       >
-        <Menu.Item
-          title="Add"
-          leadingIcon="plus"
-          onPress={() => {
-            console.log("Add");
-          }}
-        />
-        <Menu.Item
-          title="Edit"
-          leadingIcon="pencil"
-          onPress={() => {
-            console.log("Edit");
-          }}
-        />
+        <Menu.Item title="Add" leadingIcon="plus" onPress={showDialog} />
+        <Menu.Item title="Edit" leadingIcon="pencil" onPress={showDialog} />
       </Menu>
+      <Portal>
+        <Dialog visible={dialogVisible} onDismiss={hideDialog}>
+          <Dialog.Title>Future Feature</Dialog.Title>
+          <Dialog.Content>
+            <Text variant="bodyMedium">{dialogText}</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Done</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
