e0af36731442741009f139120436c9989e5ebb01
hopefully-purple <hopefully.purple@gmail.com>
2026-02-21 15:45:59 -0800

    write data to file + user can see data in Files DONE

--- file-management/movingFIles.ts
+++ file-management/movingFIles.ts
@@ -1,8 +1,8 @@
 import { File, Paths } from "expo-file-system";
 import { jsonToCSV } from "react-native-csv";
-import * as MediaLibrary from 'expo-media-library';
-import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing';
+import * as MediaLibrary from "expo-media-library";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 
 // Big picture steps:
 // 1. Don't worry about which language, just get the data for one language and write it to a file
@@ -22,13 +22,23 @@ import * as Sharing from 'expo-sharing';
 export const saveDataToCSV = async (languageObj: any) => {
   // step 1: convert JSON to string using react-native-csv
   // console.log("languageObj to be saved: " + JSON.stringify(languageObj));
-  const csv = jsonToCSV(languageObj);
-  // console.log("csv content: " + csv);
+  // Specifying fields and data explicitly
+  const csv = jsonToCSV({
+    fields: ["id", "word", "definition", "pronun", "notes"],
+    data: languageObj.words,
+  });
+  // const csv = jsonToCSV({
+  //   fields: ["Column 1", "Column 2"],
+  //   data: [
+  //     ["foo", "bar"],
+  //     ["abc", "def"],
+  //   ],
+  // });
+  // console.log("csv content: ");
+  // console.log(csv);
   // step 2: call createFile with the file name and content
-  const file = createFile("RainBucketAppFile2.csv", csv);
+  const file = createFile("RainBucketAppFile3.csv", csv);
   // step 4: upload file?
-  // console.log("call saveToMediaLibrary with file uri: " + file.uri);
-  // saveToMediaLibrary(file.uri);
   await Sharing.isAvailableAsync().then(isAvailable => {
     if (isAvailable) {
       Sharing.shareAsync(file.uri);
@@ -56,32 +66,3 @@ const createFile = (fileName: string, content: string): File => {
     throw error; // re-throw the error after logging it
   }
 };
-
-// const saveToMediaLibrary = async (fileUri: string, albumName = 'MyAppFolder') => {
-//   // Request permissions
-//   console.log("Requesting media library permissions...");
-//   const { status } = await MediaLibrary.requestPermissionsAsync();
-//   if (status !== 'granted') {
-//     console.error('Permission not granted!');
-//     return;
-//   }
-
-//   try {
-//     // Create an asset from the local file URI
-//     console.log("Creating asset from file URI");
-//     const asset = await MediaLibrary.createAssetAsync(fileUri);
-//     // Find or create a specific album (folder)
-//     console.log("Checking for album:", albumName);
-//     const album = await MediaLibrary.getAlbumAsync(albumName);
-//     if (album) {
-//       console.log("Album exists, adding asset to album");
-//       await MediaLibrary.addAssetsToAlbumAsync([asset], album.id, false);
-//     } else {
-//       console.log("Album does not exist, creating album and adding asset");
-//       await MediaLibrary.createAlbumAsync(albumName, asset, false);
-//     }
-//     console.log('File saved to Media Library album:', albumName);
-//   } catch (error) {
-//     console.error('Error saving to media library:', error);
-//   }
-// };
