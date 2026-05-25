979606a52243b169639cef6cd30c873b79271796
hopefully-purple <hopefully.purple@gmail.com>
2026-05-25 11:49:42 -0700

    GetAllKeys and GetDataFromKey

--- utilities/utility-async-storage.ts
+++ utilities/utility-async-storage.ts
@@ -17,3 +17,28 @@ export const asyncStorageSaveData = async (data: ILanguageObject): Promise<boole
     throw e;
   }
 }
+
+export const asyncStorageGetAllKeys = async (): Promise<string[]> => {
+  let keys: any = [];
+  try {
+    keys = await AsyncStorage.getAllKeys();
+    return keys;
+  } catch (e) {
+    // read key error
+    console.log("(getAllKeys) Failed to get all keys from storage: " + e);
+    throw e;
+  }
+}
+
+export const asyncStorageGetDataFromKey = async (key: string): Promise<string> => {
+  let result: any;
+  try {
+    result = await AsyncStorage.getItem(key);
+    console.log("(getDataFromKey) Result for key " + key + ": " + result);
+    return result != null ? result : "";
+  } catch (e) {
+    // read key error
+    console.log("(getDataFromKey) Failed to get data for key " + key + ": " + e);
+    throw e;
+  }
+}
